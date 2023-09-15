73dc46aaf76499c81b8d3bc1ffa8c50f38f62b63
dominik <dominik.mast.11@gmail.com>
2023-09-15 19:15:37 +0200

    tutorial done

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,95 +32,324 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool showLoginCard = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController secondPasswordController = TextEditingController();
+  bool isLoading = false;
 
-  void _incrementCounter() {
+  pageContainerReverse() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      showLoginCard = !showLoginCard;
+    });
+  }
+
+  checkRegister() {}
+
+  forgotPassword() {}
+
+  checkLogin() {
+    setState(() {
+      isLoading = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget registerCard = OwnCard(
+      title: "CREATE NEW",
+      margin: EdgeInsets.only(
+          left: 40, right: 40, top: MediaQuery.of(context).size.height * .3),
+      onTap: pageContainerReverse,
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        CustomTextField(
+          controller: emailController,
+          label: "EMAIL",
+        ),
+        CustomTextField(
+          controller: passwordController,
+          label: "PASSWORD",
+          isPasswordInput: true,
+        ),
+        CustomTextField(
+          controller: secondPasswordController,
+          label: "RE-ENTER PASSWORD",
+          isPasswordInput: true,
+        ),
+        const Expanded(child: SizedBox.shrink()),
+        if (emailController.text.isNotEmpty &&
+            passwordController.text.isNotEmpty &&
+            secondPasswordController.text.isNotEmpty)
+          TextButton(
+              onPressed: checkRegister(), child: const Text("CREATE NEW"))
+      ],
+    );
+    Widget loginCard = OwnCard(
+      title: "SIGN IN",
+      margin: EdgeInsets.only(
+          left: 20, right: 20, top: MediaQuery.of(context).size.height * .2),
+      onTap: pageContainerReverse,
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        CustomTextField(controller: emailController, label: "EMAIL"),
+        CustomTextField(
+          controller: passwordController,
+          label: "PASSWORD",
+          isPasswordInput: true,
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: TextButton(
+              onPressed: () => forgotPassword(),
+              child: const Text("Forgot password?"),
+            ),
+          ),
+        ),
+        const Expanded(child: SizedBox.shrink()),
+        if (emailController.text.isNotEmpty &&
+            passwordController.text.isNotEmpty)
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: TextButton(
+              onPressed: () => checkLogin(),
+              child: const Text(
+                "SIGN IN",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          )
+      ],
+    );
+    Widget registerCardBlanc = OwnCard(
+      title: "CREATE NEW",
+      margin: EdgeInsets.only(
+          left: 40, right: 40, top: MediaQuery.of(context).size.height * .3),
+      onTap: pageContainerReverse,
+      children: [
+        const Expanded(child: SizedBox.shrink()),
+        Container(
+          margin: const EdgeInsets.all(20),
+          child: const Text(
+            "CREATE NEW",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xFF00CCFF), Color(0xFF3366FF)],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft)),
+        child: isLoading
+            ? const LoadingCircle()
+            : Stack(
+                children: showLoginCard
+                    ? [registerCardBlanc, loginCard]
+                    : [loginCard, registerCard],
+              ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    );
+  }
+}
+
+class OwnCard extends StatelessWidget {
+  final String title;
+  final EdgeInsets margin;
+  final VoidCallback onTap;
+  final List<Widget> children;
+
+  const OwnCard({
+    super.key,
+    required this.title,
+    required this.margin,
+    required this.onTap,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: margin,
+        height: 400,
+        width: double.infinity,
+        child: Card(
+          elevation: 12,
+          color: Colors.white,
+          surfaceTintColor: Colors.transparent,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              ...children
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final void Function(String)? onChanged;
+  final EdgeInsets? margin;
+  final bool? isPasswordInput;
+
+  const CustomTextField(
+      {super.key,
+      required this.controller,
+      required this.label,
+      this.onChanged,
+      this.margin,
+      this.isPasswordInput});
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool? obscureText;
+
+  showInput() {
+    setState(() {
+      obscureText = !obscureText!;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    obscureText = widget.isPasswordInput;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: widget.margin ??
+          const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+      child: Stack(
+        children: [
+          TextField(
+            controller: widget.controller,
+            obscureText: obscureText ?? false,
+            decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                label: Text(widget.label)),
+            onChanged: widget.onChanged,
+          ),
+          if (obscureText != null)
+            Positioned(
+                right: 15,
+                top: 15,
+                child: InkWell(
+                  onTap: () => showInput(),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: Colors.grey,
+                        size: 18,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "show",
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      )
+                    ],
+                  ),
+                ))
+        ],
+      ),
+    );
+  }
+}
+
+class LoadingCircle extends StatefulWidget {
+  const LoadingCircle({super.key});
+
+  @override
+  State<LoadingCircle> createState() => _LoadingCircleState();
+}
+
+class _LoadingCircleState extends State<LoadingCircle> {
+  double width = 250;
+  double height = 250;
+  bool isStarting = false;
+
+  startAnimation() {
+    setState(() {
+      width = 100;
+      height = 100;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => startAnimation());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedContainer(
+        width: width,
+        height: height,
+        duration: const Duration(seconds: 1),
+        decoration:
+            const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        onEnd: () {
+          setState(() {
+            width = width == 100 ? 250 : 100;
+            height = height == 100 ? 250 : 100;
+          });
+        },
+      ),
     );
   }
 }
